90b65ffcac18f149d5f73a7e448ca8515752cbfc
tsubakimoto_s <tkmbmbr@gmail.com>
2014-03-12 00:32:37 +0900

    #06

--- js/app.js
+++ js/app.js
@@ -6,36 +6,18 @@ var Task = Backbone.Model.extend({
         title: 'do something!',
         completed: false
     },
-    validate: function(attrs) { // バリデーション機能
-        if (_.isEmpty(attrs.title)) { // Underscore.jsの機能を使う
-            return 'title must not be empty!';
-        }
-    },
-    toggle: function() { // メソッドも定義できる
-        this.set('completed', !this.get('completed'));
-    },
     log: function() {
         console.log(this.toJSON());
     }
 });
+var task = new Task();
 
-var task1 = new Task({
-    //title: 'do it!', // デフォルト値が設定される
-    completed: true
+// Viewの定義
+var TaskView = Backbone.View.extend({
+    tagName: 'li' // HTMLのタグ
 });
-
-// // setter
-// task1.set('title', 'newTitle');
-// // getter
-// var title = task1.get('title');
-// console.log(title);
-
-// // JSON形式で取得する
-// console.log(task1.toJSON());
-
-task1.log();
-// task1.toggle();
-task1.set({title: ''}, {validate: true}); // オブジェクト形式の引数も使える
-task1.log();
+var taskView = new TaskView({model: task});
+console.log(taskView.el); // .el : Viewを要素の形で取得する
+//console.log(taskView.$el); // .$el : jQueryのオブジェクトとなる
 
 })();
