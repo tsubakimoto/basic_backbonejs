1a4516f932a982a4d0ababe050f15e7af1a27e0d
tsubakimoto_s <tkmbmbr@gmail.com>
2014-03-12 00:21:46 +0900

    #04

--- js/app.js
+++ js/app.js
@@ -5,6 +5,12 @@ var Task = Backbone.Model.extend({
     defaults: { // デフォルト値を定義する
         title: 'do something!',
         completed: false
+    },
+    toggle: function() { // メソッドも定義できる
+        this.set('completed', !this.get('completed'));
+    },
+    log: function() {
+        console.log(this.toJSON());
     }
 });
 
@@ -13,7 +19,17 @@ var task1 = new Task({
     completed: true
 });
 
-// JSON形式で取得する
-console.log(task1.toJSON());
+// // setter
+// task1.set('title', 'newTitle');
+// // getter
+// var title = task1.get('title');
+// console.log(title);
+
+// // JSON形式で取得する
+// console.log(task1.toJSON());
+
+task1.log();
+task1.toggle();
+task1.log();
 
 })();
