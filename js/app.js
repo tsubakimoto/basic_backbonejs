da41503fd2763ce8e579af68ec0ac8390301e556
tsubakimoto_s <tkmbmbr@gmail.com>
2014-03-12 23:59:41 +0900

    #12

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 (function() {
-    
+
+var Task = Backbone.Model.extend({
+
+});
+var Tasks = Backbone.Collection.extend({ model: Task });
+
+var TaskView = Backbone.View.extend({
+
+});
+var TasksView = Backbone.View.extend({
+
+});
+
+var tasks = new Tasks([
+    { title: 'task1', completed: true },
+    { title: 'task2' },
+    { title: 'task3' }
+]);
+
 })();
