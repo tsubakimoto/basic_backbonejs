29a9d389e6cac88f0b9d35dfbfd4a48e0bbe2937
tsubakimoto_s <tkmbmbr@gmail.com>
2014-03-15 22:30:52 +0900

    #19

--- js/app.js
+++ js/app.js
@@ -44,12 +44,31 @@ var TaskView = Backbone.View.extend({
 });
 var TasksView = Backbone.View.extend({
     tagName: 'ul',
+    initialize: function() {
+        this.collection.on('add', this.addNew, this);
+    },
+    addNew: function(task) {
+        var taskView = new TaskView({ model: task });
+        this.$el.append(taskView.render().el);
+    },
     render: function() {
         this.collection.each(function(task) {
             var taskView = new TaskView({ model: task });
             this.$el.append(taskView.render().el);
         }, this);
         return this;
+    }
+});
+
+var AddTaskView = Backbone.View.extend({
+    el: '#addTask',
+    events: {
+        'submit': 'submit'
+    },
+    submit: function(e) {
+        e.preventDefault();
+        var task = new Task({ title: $('#title').val() });
+        this.collection.add(task);
     }
 });
 
@@ -60,6 +79,7 @@ var tasks = new Tasks([
 ]);
 
 var tasksView = new TasksView({ collection: tasks });
+var addTaskView = new AddTaskView({ collection: tasks });
 $('#tasks').html(tasksView.render().el);
 
 })();
